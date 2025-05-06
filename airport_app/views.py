e8c89d0d4c6ab7575866ef9e6788f179811d5a74
Andrii <yurkivandriy02@gmail.com>
2025-05-07 00:27:09 +0300

    Add ModelViewSet for all models

--- airport_app/views.py
+++ airport_app/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport_app.models import (
+    Country,
+    City,
+    Airport,
+    Route,
+    Airplane,
+    Flight,
+    Ticket,
+    Order
+)
+from airport_app.serializers import (
+    CountrySerializer,
+    CitySerializer,
+    AirportSerializer,
+    RouteSerializer,
+    AirplaneSerializer,
+    FlightSerializer,
+    TicketSerializer,
+    OrderSerializer
+)
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
